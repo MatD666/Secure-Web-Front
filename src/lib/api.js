e3a8c3577c59318e3b363de10eb99602ba1cc6f2
MatD666 <105509212+MatD666@users.noreply.github.com>
2023-02-18 23:18:44 +0100

    Update api.js

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,6 +1,6 @@
 import { error } from '@sveltejs/kit';
 
-const base = 'http://localhost:3000';
+const base = 'https://secure-web-dev-backend-c25z.onrender.com';
 
 async function send({ method, path, data, token }) {
 	const opts = { method, headers: {} };
